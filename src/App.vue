fdac3d5298af318f61c2853cfdcdf6e861bbd21a
Adam Harpur <adam@harpur.io>
2022-12-15 21:08:35 +0000

    initial answer layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Answers from "./components/Answers.vue";
+import Scores from "./components/Scores.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <section class="flex">
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper"></div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <Answers />
+      <Scores />
+    </main>
+  </section>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.flex {
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Answers.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+const answers = [
+  {
+    content: "Australia",
+    points: 26,
+  },
+  {
+    content: "Afghanistan",
+    points: 21,
+  },
+  {
+    content: "America",
+    points: 18,
+  },
+  {
+    content: "Albania",
+    points: 13,
+  },
+  {
+    content: "Algeria",
+    points: 6,
+  },
+  {
+    content: "Argentina",
+    points: 4,
+  },
+  {
+    content: "Armenia",
+    points: 2,
+  },
+];
+</script>
+<template>
+  <div class="flex" v-for="(answer, position) in answers" :key="answer.content">
+    <div class="position">{{ position + 1 }}</div>
+    <div class="content">{{ answer.content }}</div>
+    <div class="points">{{ answer.points }}</div>
+  </div>
+</template>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+
+.flex > div {
+  padding: 1rem;
+}
+.position {
+  flex-basis: 8%;
+  max-width: 8%;
+  background-color: red;
+}
+.content {
+  flex-basis: 80%;
+  max-width: 80%;
+  background-color: blue;
+}
+
+.points {
+  flex-basis: 12%;
+  max-width: 12%;
+  background-color: pink;
+}
+</style>
