25f7b90be2e050bbb86a8bb2bfd2f706faa56796
Adam Harpur <adam@harpur.io>
2022-12-19 16:06:28 +0000

    add questions

---
+++ src/game.fixture.ts
@@ -0,0 +1,331 @@
+export const game = [
+  {
+    question: "Name a country that's name starts with the letter A",
+    answers: [
+      {
+        content: "Australia",
+        points: 26,
+      },
+      {
+        content: "Afghanistan",
+        points: 21,
+      },
+      {
+        content: "America",
+        points: 18,
+      },
+      {
+        content: "Albania",
+        points: 13,
+      },
+      {
+        content: "Algeria",
+        points: 6,
+      },
+      {
+        content: "Argentina",
+        points: 4,
+      },
+      {
+        content: "Armenia",
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: "Name Something You Would Hate to Find Under Your Bed",
+    answers: [
+      {
+        content: "Monster",
+        points: 43,
+      },
+      {
+        content: "Snake",
+        points: 16,
+      },
+      {
+        content: "Food",
+        points: 12,
+      },
+      {
+        content: "Spider",
+        points: 13,
+      },
+      {
+        content: "Rodent",
+        points: 4,
+      },
+    ],
+  },
+  {
+    question: "Name Something People Do While Riding a Rollercoaster",
+    answers: [
+      {
+        content: "Scream",
+        points: 43,
+      },
+      {
+        content: "Hold Arms Up",
+        points: 32,
+      },
+      {
+        content: "Get Sick",
+        points: 13,
+      },
+      {
+        content: "Laugh",
+        points: 4,
+      },
+      {
+        content: "Close Their Eyes",
+        points: 4,
+      },
+    ],
+  },
+  {
+    question: "Name an ice cream flavour",
+    answers: [
+      {
+        content: "Vanilla",
+        points: 36,
+      },
+      {
+        content: "Chocolate",
+        points: 20,
+      },
+      {
+        content: "Mint Choc Chip",
+        points: 20,
+      },
+      {
+        content: "Pistachio",
+        points: 13,
+      },
+      {
+        content: "Strawberry",
+        points: 8,
+      },
+      {
+        content: "Raspberry Ripple",
+        points: 6,
+      },
+    ],
+  },
+  {
+    question: "Name a word that has key in it.",
+    answers: [
+      {
+        content: "Keyboard",
+        points: 36,
+      },
+      {
+        content: "Keychain",
+        points: 26,
+      },
+      {
+        content: "Monkey",
+        points: 20,
+      },
+      {
+        content: "House key",
+        points: 14,
+      },
+      {
+        content: "Keystone",
+        points: 4,
+      },
+    ],
+  },
+  {
+    question:
+      "Name an article of clothing not suitable for the washing machine",
+    answers: [
+      {
+        content: "Shoe",
+        points: 29,
+      },
+      {
+        content: "Bra",
+        points: 21,
+      },
+      {
+        content: "Hat",
+        points: 16,
+      },
+      {
+        content: "Coat",
+        points: 13,
+      },
+      {
+        content: "Jumper",
+        points: 10,
+      },
+      {
+        content: "Suit",
+        points: 9,
+      },
+    ],
+  },
+  {
+    question: "Name something people are often chased by in movies",
+    answers: [
+      {
+        content: "Monsters",
+        points: 37,
+      },
+      {
+        content: "Cars",
+        points: 22,
+      },
+      {
+        content: "Police",
+        points: 19,
+      },
+      {
+        content: "Bad Guys",
+        points: 10,
+      },
+      {
+        content: "Dogs",
+        points: 10,
+      },
+    ],
+  },
+  {
+    question: "Name something a customer would do to annoy a waitress",
+    answers: [
+      {
+        content: "Send Food Back",
+        points: 40,
+      },
+      {
+        content: "Not Tip",
+        points: 35,
+      },
+      {
+        content: "Flirt",
+        points: 6,
+      },
+      {
+        content: "Snap Fingers",
+        points: 5,
+      },
+      {
+        content: "Spill Something",
+        points: 4,
+      },
+    ],
+  },
+  {
+    question: "Name an animal you can't fit in a mini cooper",
+    answers: [
+      {
+        content: "Elephant",
+        points: 81,
+      },
+      {
+        content: "Giraffe",
+        points: 17,
+      },
+      {
+        content: "Blue Whale",
+        points: 7,
+      },
+      {
+        content: "Rhino",
+        points: 5,
+      },
+      {
+        content: "Hippo",
+        points: 5,
+      },
+      {
+        content: "Horse",
+        points: 4,
+      },
+    ],
+  },
+  {
+    question: "Name a sport played with a ball",
+    answers: [
+      {
+        content: "Football",
+        points: 35,
+      },
+      {
+        content: "Tennis",
+        points: 22,
+      },
+      {
+        content: "Cricket",
+        points: 14,
+      },
+      {
+        content: "Rugby",
+        points: 11,
+      },
+      {
+        content: "Basketball",
+        points: 9,
+      },
+      {
+        content: "Netball",
+        points: 7,
+      },
+    ],
+  },
+  {
+    question: "Name Someone or Something that has fangs",
+    answers: [
+      {
+        content: "Vampire",
+        points: 34,
+      },
+      {
+        content: "Snake",
+        points: 28,
+      },
+      {
+        content: "Wolf",
+        points: 17,
+      },
+      {
+        content: "Lion / Tiger",
+        points: 12,
+      },
+      {
+        content: "Werewolf",
+        points: 9,
+      },
+    ],
+  },
+  {
+    question: "Name a ride you might see at a fun fair",
+    answers: [
+      {
+        content: "Dodgems",
+        points: 50,
+      },
+      {
+        content: "Ferris Wheel",
+        points: 32,
+      },
+      {
+        content: "Waltzer / Tea Cups",
+        points: 27,
+      },
+      {
+        content: "Carousel",
+        points: 17,
+      },
+      {
+        content: "Rollercoaster",
+        points: 15,
+      },
+      {
+        content: "Chost Train",
+        points: 7,
+      },
+    ],
+  },
+];
